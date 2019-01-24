fc76acfd4686c2c3a7efd01420c82f1ff30a383a
Thao Tran <thao.tran@wellbarrier.com>
2019-01-24 10:09:16 +0700

    Updates

---
+++ precache-manifest.63a322984262814a0b0d5bc21ab2efba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-deploy-github-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "425e50c1870934821036",
+    "url": "/demo-deploy-github-pages/static/js/runtime~main.425e50c1.js"
+  },
+  {
+    "revision": "3724fbbf5477154b5934",
+    "url": "/demo-deploy-github-pages/static/js/main.3724fbbf.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/demo-deploy-github-pages/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3724fbbf5477154b5934",
+    "url": "/demo-deploy-github-pages/static/css/main.ed593dec.chunk.css"
+  },
+  {
+    "revision": "bd452d6e6b13d56dc94547c55b501add",
+    "url": "/demo-deploy-github-pages/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/demo-deploy-github-pages/precache-manifest.63a322984262814a0b0d5bc21ab2efba.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/demo-deploy-github-pages/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
